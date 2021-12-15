4e91fa01a0c966d2263982a0ad9a6c3d9bd51631
name <cybersteer@naver.com>
2021-12-15 12:54:44 +0900

    color add

--- time.js
+++ time.js
@@ -1,19 +1,28 @@
 function leftTime(finish){
-    let year =finish.slice(0,4);
-    let month =finish.slice(5,7);
-    let day =finish.slice(8,10);
-    let hour =finish.slice(13,15);
-    let minute =finish.slice(16,18);
-    let newTime = new Date(year,month-1,day,hour,minute);
+    let year =finish.slice(0,2);
+    let month =finish.slice(3,5);
+    let day =finish.slice(6,8);
+    let hour =finish.slice(10,12);
+    let minute =finish.slice(13,15);
+    let newTime = new Date("20"+year,month-1,day,hour,minute);
     let nowTime = new Date();
     let difference = newTime-nowTime;
     let result;
-    if(parseInt(parseInt(difference/1000/60)/60/24/365)==0){ //년
+    if(difference<=0){
+        return "만료";
+    }
+    else if(parseInt(parseInt(difference/1000/60)/60/24/365)==0){ //년
         if(parseInt(parseInt(difference/1000/60/60/24)/30)==0){ //달
             if(parseInt(difference/1000/60/60/24)==0){ //일
                 if(parseInt(difference/1000/60/60)==0){ //시
-                        result = parseInt(difference/1000/60)+1;
+                    if(parseInt(difference/1000/60)==0){ //분
+                        result = parseInt(difference/1000);
+                        return result+"초";
+                    }
+                    else{
+                        result = parseInt(difference/1000/60);
                         return result+"분";
+                    }
                 }
                 else{
                     result = parseInt(difference/1000/60/60);
